91daf6f754307e0fc01fa5496a94371859e22b9e
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2020-04-11 19:05:55 -0700

    Permit more fields in create Event API

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,6 +46,6 @@ class EventsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def event_params
-      params.require(:event).permit(:id, :name, :date)
+      params.require(:event).permit(:id, :name, :time, :source, :source_id, :details)
     end
 end

1b844a2616654aae9dfb82b64c51017b259f84e3
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2020-08-01 16:34:13 -0700

    Add start time support with default to events index action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 
   # GET /events
   def index
-    @events = Event.all
+    @events = Event.where('time > ?', params[:start_time]&.to_time || 1.year.ago)
 
     render json: @events
   end
